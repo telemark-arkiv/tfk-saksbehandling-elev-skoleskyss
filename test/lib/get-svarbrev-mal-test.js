cf35cddc11e44df8a0d4ae9a141acdd3a2a8fb89
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-06-15 13:18:00 +0200

    Updates svarbrev

--- test/lib/get-svarbrev-mal-test.js
+++ test/lib/get-svarbrev-mal-test.js
@@ -1,5 +1,22 @@
 'use strict'
 
-var tap = require('tap')
-var getSvarbrevMal = require('../../lib/get-svarbrev-mal')
+const tap = require('tap')
+const getSvarbrevMal = require('../../lib/get-svarbrev-mal')
+const svarbrevInnvilget = require('../data/svarbrev-innvilget.json')
+const svarbrevAvslag = require('../data/svarbrev-avslag.json')
+const svarbrevManuell = require('../data/svarbrev-manuell.json')
+const svarbrevManuellHybel = require('../data/svarbrev-manuell-hybel.json')
+const svarbrevManuellDelt = require('../data/svarbrev-manuell-delt.json')
+const svarbrevManuellAnnen = require('../data/svarbrev-manuell-annen.json')
 
+tap.equal(getSvarbrevMal(svarbrevInnvilget), 'innvilget', 'It returns innvilget for innvilget')
+
+tap.equal(getSvarbrevMal(svarbrevAvslag), 'avslag', 'It returns avslag for avslag')
+
+tap.equal(getSvarbrevMal(svarbrevManuell), 'midlertidig-manuell', 'It returns midlertidig-manuell for midlertidig-manuell')
+
+tap.equal(getSvarbrevMal(svarbrevManuellHybel), 'midlertidig-hybel', 'It returns midlertidig-hybel for midlertidig-hybel')
+
+tap.equal(getSvarbrevMal(svarbrevManuellDelt), 'midlertidig-delt', 'It returns midlertidig-delt for midlertidig-delt')
+
+tap.equal(getSvarbrevMal(svarbrevManuellAnnen), 'midlertidig-annen', 'It returns midlertidig-annen for midlertidig-annen')
