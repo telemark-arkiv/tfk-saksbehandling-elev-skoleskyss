fc9d687e0e3f2192779bc91051ff722df85ac84a
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-06-15 10:13:26 +0200

    Adds test for age. Updates setup-item

--- test/lib/automatic-yes-test.js
+++ test/lib/automatic-yes-test.js
@@ -5,9 +5,12 @@ const autoYes = require('../../lib/automatic-yes')
 const autoYesTrueFolkeregistrert = require('../data/automatisk-ja-folkeregistrert-over.json')
 const autoYesTrueDelt = require('../data/automatisk-ja-delt-over.json')
 const autoYesTrueFerry = require('../data/automatisk-ja-baat.json')
+const autoYesFalseAge = require('../data/automatisk-ja-false-alder.json')
 
 tap.equal(autoYes(autoYesTrueFolkeregistrert), true, 'It returns yes for folkeregistert adresse if >= 6200')
 
 tap.equal(autoYes(autoYesTrueDelt), true, 'It returns yes for delt adresse if folkeregistrert is >= 6200')
 
 tap.equal(autoYes(autoYesTrueFerry), true, 'It returns yes for Boat/ferry')
+
+tap.equal(autoYes(autoYesFalseAge), false, 'It returns false for age > 26')
